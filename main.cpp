52b7a8e153a4d701a29c935373df6e113c9edc2e
heini-phys-chem <heini.phys.chem@gmail.com>
2019-04-08 15:22:05 +0200

    removed commented out stuff

--- main.cpp
+++ main.cpp
@@ -8,16 +8,6 @@
 int main (void) 
 {
 
-//  // define test vectors and matrices
-//	std::vector<double> x = {1.2,2.2,1.3,3.1};
-//  std::vector<double> y = {2.2,1.5,5.,3.};
-//  std::vector<std::vector<double>> matrix = {
-//    {1,2,3},
-//    {4,5,6},
-//    {7,8,9},
-//    {10,11,12}
-//  };
-  
   // get number of nRows and nCols
   int nRow = matrix.size();
   int nCol = matrix[0].size();
